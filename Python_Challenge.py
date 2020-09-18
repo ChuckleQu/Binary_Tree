42cbd8fa8dd9bbce29b5e2dbcb7b25e717f4e418
ChuckleQu <69857410+ChuckleQu@users.noreply.github.com>
2020-09-18 23:08:39 +0800

    Update Python_Challenge.py

--- Python_Challenge.py
+++ Python_Challenge.py
@@ -9,7 +9,7 @@ for n in range(0, 100):                      # Create 100 random numbers and sto
 my_tree = BinaryTree(list_of_numbers)        # Initialize "list_of_numbers": equip it with "LeftChild" and "RightChild".
 my_tree = create_tree(my_tree)               # Call function "create_tree" to create the tree.
 
-print('node\tmin\t\tmax\t\tnum_of_points')   # Print the title.
+print('node\t\tmin\t\tmax\t\tnum_of_points')   # Print the title.
 print_tree(my_tree)                          # Call function "print_tree" to print out the required information.
 
 # Note that when print out "my_tree", I place a blank line when one level is end.
