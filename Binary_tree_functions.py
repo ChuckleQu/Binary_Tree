8aaf69dea548aa9ee83c8a3cf4e9c59422aed951
ChuckleQu <69857410+ChuckleQu@users.noreply.github.com>
2020-09-19 08:56:23 +0800

    Add files via upload

---
+++ Binary_tree_functions.py
@@ -0,0 +1,61 @@
+class BinaryTree:
+    def __init__(self, rootObj, name='X'):           # Make change here: add "name" to __init__.
+        self.root = rootObj
+        self.name = name
+        self.leftChild = None
+        self.rightChild = None
+
+    def add_level(self):
+        length = max(self.root) - min(self.root)
+        half_length = length / 2
+        left = [num for num in self.root if num <= min(self.root) + half_length]
+        right = [num for num in self.root if num > max(self.root) - half_length]
+        self.leftChild = BinaryTree(left, self.name + 'L')       # For the "leftChild", the name will plus "L"
+        self.rightChild = BinaryTree(right, self.name + 'R')     # For the "rightChild", the name will plus "R"
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def getRootValue(self):
+        return self.root
+
+
+def create_tree(tree):
+    if len(tree.getRootValue()) > 5:
+        tree.add_level()
+        create_tree(tree.getLeftChild())
+        create_tree(tree.getRightChild())
+    return tree
+
+
+def print_tree(root):
+    queue = [root]
+    toBePrintedNodes = 1
+    nextLevelNodes = 0
+
+    while len(queue) > 0:
+        currentRoot = queue.pop(0)
+        print(f'{currentRoot.name}\t\t{min(currentRoot.root)}\t\t{max(currentRoot.root)}\t\t{len(currentRoot.root)}')
+        # Make change here: we print "currentRoot.name" instead of "nodeNumber"
+
+        if currentRoot.leftChild:
+            queue.append(currentRoot.leftChild)
+            nextLevelNodes += 1
+
+        if currentRoot.rightChild:
+            queue.append(currentRoot.rightChild)
+            nextLevelNodes += 1
+
+        toBePrintedNodes -= 1
+
+        if toBePrintedNodes == 0:
+            if nextLevelNodes == 0:
+                break
+            print()
+            toBePrintedNodes = nextLevelNodes
+            nextLevelNodes = 0
+
+# Execute python<yourPythonFile.py>outputfile.txt at Terminal to get txt file of the output.
