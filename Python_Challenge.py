4980a7ba9a63181467af5b962b4dde17a72f9d54
ChuckleQu <69857410+ChuckleQu@users.noreply.github.com>
2020-09-19 13:24:28 +0800

    Add files via upload

---
+++ Python_Challenge.py
@@ -0,0 +1,15 @@
+from Binary_Tree_Functions import BinaryTree, create_tree, print_tree, create_numbers
+
+list_of_numbers = create_numbers()           # Create 100 random numbers and store it in "list_of_numbers".
+
+my_tree = BinaryTree(list_of_numbers, 'X')   # Initialize "list_of_numbers": equip it with "LeftChild" and "RightChild".
+my_tree = create_tree(my_tree)               # Call function "create_tree" to create the tree.
+
+print('node\t\tmin\t\tmax\t\tnum_of_points')  # Print the title.
+print_tree(my_tree)                           # Call function "print_tree" to print out the required information.
+
+# Note that when print out "my_tree", I place a blank line when one level is end.
+# This can help us to identify different levels.
+# If you don't like this blank line, you can also delete it by deleting the line 58 in "Binary_tree_function.py" file.
+
+# Execute python<yourPythonFile.py>outputfile.txt at Terminal to get txt file of the output.
