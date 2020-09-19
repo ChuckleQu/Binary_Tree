799561ddfbd0e5cec5e61345b6c7e87c96cc8837
ChuckleQu <69857410+ChuckleQu@users.noreply.github.com>
2020-09-19 08:45:49 +0800

    Update Python_Challenge.py

--- Python_Challenge.py
+++ Python_Challenge.py
@@ -10,7 +10,7 @@ my_tree = BinaryTree(list_of_numbers)        # Initialize "list_of_numbers": equ
 my_tree = create_tree(my_tree)               # Call function "create_tree" to create the tree.
 
 print('node\t\tmin\t\tmax\t\tnum_of_points')   # Print the title.
-print_tree(my_tree)                          # Call function "print_tree" to print out the required information.
+print_tree(my_tree)                            # Call function "print_tree" to print out the required information.
 
 # Note that when print out "my_tree", I place a blank line when one level is end.
 # This can help us to identify different levels.
